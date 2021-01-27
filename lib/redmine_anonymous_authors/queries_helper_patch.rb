ab3831e2b23a02b064446517ac7e3cf1607b7556
Kostas Koumpouras <kkoumpouras@gmail.com>
2021-01-27 18:55:54 +0100

    Fix queries_helper_patch

--- lib/redmine_anonymous_authors/queries_helper_patch.rb
+++ lib/redmine_anonymous_authors/queries_helper_patch.rb
@@ -5,6 +5,7 @@ module RedmineAnonymousAuthors
     def self.included(base)
       base.send(:include, InstanceMethods)
       base.class_eval do
+        alias_method :column_value_without_anonymous, :column_value
         alias_method :column_value, :column_value_with_anonymous
       end
     end
